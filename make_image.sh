5100b0e86d8e3dc8e085f48d8fb481481c53fb0c
Daniel Augustin <hossie@hossie.de>
2021-08-16 14:20:40 +0200

    Use mountpoint instead of volume id

--- make_image.sh
+++ make_image.sh
@@ -2,11 +2,12 @@
 set -ex
 
 buildcmd() {
-  buildah run -v "${VOLUME_ID}":/var/db/repos/gentoo --network host "${c}" -- "$@"
+  buildah run -v "${MOUNT_POINT}":/var/db/repos/gentoo --network host "${c}" -- "$@"
 }
 
 podman run -d gentoo/portage
 VOLUME_ID="$(podman volume list | tail -n1 | awk '{print $2}')"
+MOUNT_POINT="$(podman volume inspect $VOLUME_ID | jq -r '.[].Mountpoint')"
 c=$(buildah from gentoo/stage3)
 
 buildcmd mkdir -p /repo
